088826612e170515d135b37d1696b99cd8378ce5
Jason Reich <jason@swiftorific.co.uk>
2025-12-03 10:53:18 +0000

    Day 3 Part 2 doesn't work

--- 03/main.py
+++ 03/main.py
@@ -1,20 +1,26 @@
-# example = """
-# 987654321111111
-# 811111111111119
-# 234234234234278
-# 818181911112111
-# """.strip().splitlines()
+example = """
+987654321111111
+811111111111119
+234234234234278
+818181911112111
+""".strip().splitlines()
 
 with open("03/input.txt") as f:
     input = f.readlines()
 
 total = 0
-for bank in input:
-    combos = [
-        int(bank[i] + bank[j])
-        for i in range(0, len(bank))
-        for j in range(i + 1, len(bank))
-    ]
-    total += max(combos)
+for bank in [input[0]]:
+    output = list("000000000000")
+    i = 0
+    for c in range(0, 12):
+        m = max(bank[i:len(bank) - 11 + c])
+        i = min([
+            j
+            for j in range(i, len(bank) - 11 + c)
+            if bank[j] == m
+        ]) + 1
+        output[c] = m[0]
+
+    total += int(str(''.join(output)))
 
 print(total)
