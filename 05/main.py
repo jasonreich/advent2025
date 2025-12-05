dde2fc23286b5fcc739dbc1b4588ad0ba2743e27
Jason Reich <jason@swiftorific.co.uk>
2025-12-05 10:41:14 +0000

    Not working

--- 05/main.py
+++ 05/main.py
@@ -25,14 +25,25 @@ with open("05/input.txt") as f:
 pattern = "([0-9]+)\\-([0-9]+)"
 
 intervals = [
-    range(int(match.group(1)), int(match.group(2)) + 1)
+    (int(match.group(1)), int(match.group(2)))
     for interval in raw_intervals
     if (match := re.search(pattern, interval))
 ]
 
+intervals.sort()
+
+bound = -1
+
 count = 0
-for ingredient in ingredients:
-    if any(int(ingredient) in interval for interval in intervals):
-        count+=1
+
+for (low, high) in intervals:
+    n = max(0, bound - low)
+
+    print(f"{bound} : {low} - {high} (remove {n})")
+    
+    count += (high - low + 1) - n
+
+    bound = max(high, bound) + 1
+
 
 print(count)
