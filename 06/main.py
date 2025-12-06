aacde6252fea9adf12b4c65b99a975c5dfae80b2
Jason Reich <jason@swiftorific.co.uk>
2025-12-06 11:17:57 +0000

    Day 6 Part 2

--- 06/main.py
+++ 06/main.py
@@ -1,12 +1,10 @@
 import numpy as np
 import re
 
-example = """
-123 328  51 64 
+example = """123 328  51 64 
  45 64  387 23 
   6 98  215 314
-*   +   *   +  
-""".strip()
+*   +   *   +  """
 
 with open("06/input.txt") as f:
     from_file = f.read()
@@ -14,24 +12,49 @@ with open("06/input.txt") as f:
 input = np.array([
     [
         cell
-        for cell in re.split('\\s+', line)
-        if cell.strip() != ""
+        for cell in line
     ]
     for line in from_file.splitlines()
-])
+]).transpose()
 
-def process(arr):
-    operator = arr[-1]
-    values = arr[:-1].astype(int)
-    if operator == "*":
-        return values.prod()
+(rows, cols) = input.shape
+
+operator = ''
+acc = 0
+total = 0
+for i in range(0, rows):
+    row = input[i]
+    if row[-1].strip() != '':
+        operator = row[-1]
+        if operator == '*':
+            acc = 1
+        elif operator == '+':
+            acc = 0
+    
+    value = ''.join(row[0:-1]).strip()
+    if value != '':
+        if operator == '*':
+            acc *= int(value)
+        elif operator == '+':
+            acc += int(value)
     else:
-        return values.sum()
+        total += acc
 
-output = np.apply_along_axis(
-    process,
-    0,
-    input
-).sum()
+total += acc
+print(total)
 
-print(output)
+# def process(arr):
+#     operator = arr[-1]
+#     values = arr[:-1].astype(int)
+#     if operator == "*":
+#         return values.prod()
+#     else:
+#         return values.sum()
+
+# output = np.apply_along_axis(
+#     process,
+#     0,
+#     input
+# ).sum()
+
+# print(output)
