5a4a0962aef7af4f5549fd4d1818eb71601508dc
Jason Reich <jason@swiftorific.co.uk>
2025-12-09 12:48:00 +0000

    I don't understand why this doesn't work

--- 09/main.py
+++ 09/main.py
@@ -1,3 +1,4 @@
+from PIL.Image import enum
 from scipy.spatial import ConvexHull
 from matplotlib.path import Path
 
@@ -19,56 +20,24 @@ type coord = tuple[int, int]
 
 input: list[coord] = [
     (int(x), int(y))
-    for line in example.splitlines()
+    for line in from_file.splitlines()
     for [x, y] in [line.split(',')]
 ]
 
 hull = ConvexHull(input)
 path_obj = Path(hull.points[hull.vertices])
 
-lower_x = min([ x for (x, _) in input ])
-lower_y = min([ y for (_, y) in input ])
-upper_x = max([ x for (x, _) in input ])
-upper_y = max([ y for (_, y) in input ])
+areas = [
+    (abs(y1-y0) + 1) * (abs(x1 - x0) + 1)
+    for i, (x0, y0) in enumerate(input)
+    for (x1, y1) in input[i+1:]
+    if path_obj.contains_path(Path([
+        (x0, y0),
+        (x0, y1),
+        (x1, y1), 
+        (x1, y0),
+        (x0, y0),
+    ]))
+]
 
-mid_x = (upper_x - lower_x) // 2
-mid_y = (upper_y - lower_y) // 2
-
-x0 = mid_x
-x1 = mid_x
-y0 = mid_y
-y1 = mid_y
-
-while True:
-    if path_obj.contains_path(Path([(x0 - 1, y0), (x0 - 1, y1), (x1, y1), (x1, y0), (x0)])):
-        x0 -= 1
-    elif path_obj.contains_path(Path([(x0, y0 - 1), (x0, y1), (x1, y1), (x1, y0 - 1)])):
-        y0 -= 1
-    elif path_obj.contains_path(Path([(x0, y0), (x0, y1), (x1 + 1, y1), (x1 + 1, y0)])):
-        x1 += 1
-    elif path_obj.contains_path(Path([(x0, y0), (x0, y1 + 1), (x1, y1 + 1), (x1, y0)])):
-        y1 += 1
-    else:
-        print((x0, y0), (x1, y1))
-        print((abs(y1-y0) + 1) * (abs(x1 - x0) + 1))
-        break
-    
-
-# areas = [
-#     (abs(y1-y0) + 1) * (abs(x1 - x0) + 1)
-#     for x0 in range(lower_x, upper_x + 1)
-#     for y0 in range(lower_y, upper_y + 1)
-#     if path_obj.contains_point((x0, y0))
-#     for x1 in range(x0, upper_x + 1)
-#     if path_obj.contains_point((x0, y0))
-#     if path_obj.contains_point((x1, y0))
-#     for y1 in range(y0, upper_y + 1)
-#     if path_obj.contains_path(Path([
-#         (x0, y0),
-#         (x0, y1),
-#         (x1, y1), 
-#         (x1, y0)
-#     ]))
-# ]
-
-# print(max(areas))
+print(max(areas))
